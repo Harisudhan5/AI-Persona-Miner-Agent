09073a67db5a506873e533fa46c17ed36ffbdd16
Harisudhan5 <speaktoharisudhan@gmail.com>
2024-06-28 20:58:38 +0530

    modified data extractor

--- data_extractor.py
+++ data_extractor.py
@@ -1,17 +1,28 @@
 import requests
 from googlesearch import search
+from linkedin_api import Linkedin
+from dotenv import load_dotenv
+import os 
 
+load_dotenv()
+
+# Data Extractor 
 
 url = "https://verchoolfoz.vercel.app/next/api/user/detail/LB2giDDub4OpoWLG2ZPGT8eD56i1"
 
 response = requests.get(url)
 
 if response.status_code == 200:
-    # Parse the JSON data
     data = response.json()['data']
     print("Input Data : ",data)
 else:
     print(f"Failed to retrieve data: {response.status_code}")
+
+def linkedin_scrape(url):
+    parameter_1, parameter_2 = os.getenv('KEY1'), os.getenv('KEY2')
+    api = Linkedin(parameter_1, parameter_2)
+    profile = api.get_profile(url)
+    return profile
 
 def search_web(query):
     url_array = []
@@ -19,6 +30,17 @@ def search_web(query):
     for url in result:
         url_array.append(url)
     return url_array
+
+def web_crawl(web_url):
+    base_url = 'https://r.jina.ai/'
+    input_url = web_url
+    url = base_url + input_url
+    response = requests.get(url)
+    if response.status_code == 200:
+        return str(response.content)  
+    else:
+        return ""
+
 
 print("User Name : ",data['name'])
 print("User Company : ",data['company'])
@@ -28,6 +50,7 @@ name = data['name']
 company = data['company']
 position = data['position']
 
+# For custom testing 
 #name = data['name']
 #company = data['company']
 #position = data['position']
